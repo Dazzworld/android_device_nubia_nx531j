81316c4a1172a28245f41f7712ab53dac7c85738
chenzc <dazzworld@163.com>
2019-03-10 15:10:07 +0800

    nx531j: bring up audio

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -28,8 +28,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.vc_call_vol_steps=7 \
     ro.vendor.audio.sdk.fluencetype=fluence \
     ro.vendor.audio.sdk.ssr=false \
-    vendor.audio.dolby.ds2.enabled=false \
-    vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio.dolby.ds2.enabled=true \
+    vendor.audio.dolby.ds2.hardbypass=true \
     vendor.audio_hal.period_size=192 \
     vendor.audio.hw.aac.encoder=true \
     vendor.audio.offload.buffer.size.kb=64 \
@@ -42,6 +42,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.safx.pbe.enabled=true \
     vendor.audio.tunnel.encode=false \
     vendor.voice.path.for.pcm.voip=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio.effect.name=dolby \
+    audio.dolby.ds2.enabled=true \
+    dmid=3067269873 \
+    audio.ds1.metainfo.key=273
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
