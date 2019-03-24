0f0fe4345341cef97b4ccb2c80a6dc70f719151e
chenzc <dazzworld@163.com>
2019-03-24 12:55:12 +0800

    nx531j: fix the wrong value

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -126,7 +126,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
-    ro.vendor.qti.sys.fw.bg_apps_limit=60e
+    ro.vendor.qti.sys.fw.bg_apps_limit=60
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
