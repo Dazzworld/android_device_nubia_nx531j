8c30a17f7a004f001a37c0a240ebf9bc8831d901
chenzc <dazzworld@163.com>
2019-03-24 14:40:00 +0800

    nx531j: Add missing thermal-config file

--- msm8996.mk
+++ msm8996.mk
@@ -372,6 +372,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.mokee.trust@1.0-service
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf \
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
